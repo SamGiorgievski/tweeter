fcacecff2ef2568aa4b8f60945db533cb23a9e5f
Samuel Giorgievski <samgiorgievski@gmail.com>
2022-12-07 17:48:17 -0500

    Created event listener for new tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,15 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const createTweetElement = function(tweetObject) {
 
-// Generate the DOM structure for a tweet
-
-
-
-
-const createTweetElement = function (tweetObject) {
-
-    let tweetArticle = `
+  let tweetArticle = `
     <article class="tweet">
       
       <div class="tweet-header">
@@ -37,51 +31,73 @@ const createTweetElement = function (tweetObject) {
       </div>
       </div>
     </article>
-    `
+    `;
 
-return tweetArticle;
-}
+  return tweetArticle;
+};
 
 
 
-const renderTweets = function (tweets) {
+const renderTweets = function(tweets) {
 
   for (let tweet of tweets) {
     let tweetElement = createTweetElement(tweet);
-    $('.new-tweet').append(tweetElement); 
+    $('.new-tweet').append(tweetElement);
   }
 
-return;
-}
+  return;
+};
 
 
 // temp data
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+// const data = [
+//   {
+//     "user": {
+//       "name": "Newton",
+//       "avatars": "https://i.imgur.com/73hZDYK.png"
+//       ,
+//       "handle": "@SirIsaac"
+//     },
+//     "content": {
+//       "text": "If I have seen further it is by standing on the shoulders of giants"
+//     },
+//     "created_at": 1461116232227
+//   },
+//   {
+//     "user": {
+//       "name": "Descartes",
+//       "avatars": "https://i.imgur.com/nlhLi3I.png",
+//       "handle": "@rd"
+//     },
+//     "content": {
+//       "text": "Je pense , donc je suis"
+//     },
+//     "created_at": 1461113959088
+//   }
+// ];
 
 
-$( document ).ready(function () {
+$(document).ready(function() {
   renderTweets(data);
-})
+
+
+  $('#tweet-form').on("submit", function (event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+
+    $.post('/tweets', data)
+      .then(() => {
+        console.log("success");
+
+      });
+  });
+
+  function loadTweets () {
+
+    $.get('/tweets') 
+
+  }
+
+  loadTweets();
+
+});
